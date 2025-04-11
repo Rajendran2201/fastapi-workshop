51caac373dff1ccadb4bd5ede5e4408a8b54c802
Raj <asrajendrayadav@gmail.com>
2025-04-11 12:20:37 +0530

    basemodel usage

--- app.py
+++ app.py
@@ -1,8 +1,39 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+
+class UserModel(BaseModel):
+    username: str
+    password: str
 
 app = FastAPI()
 
-@app.get("/")
-async def home():
-    return {"message": "Your FastAPI server is running"}, 200
+db = {"users":[{"default":"default", "rajendran":"raj2201"}]}
 
+@app.get("/user")
+async def user():
+    return db
+
+@app.post("/user")
+async def post_user(user: UserModel, status_code=201):
+    username = user.username
+    password = user.password
+    db["users"].append({username:password})
+    return {"message": "User created successfully"}
+
+@app.get("/user/{username}")
+async def get_user_by_username(username:str):
+    found = False
+    for user in db["users"]:
+        if user.get(username):
+            found = True
+            break
+    if found:
+        return JSONResponse(
+            content={"message": f"User: {username} is found in the DB"},
+                     status_code=200,
+                     media_type="application/json")
+    return JSONResponse(
+            content={"message": f"User: {username} is not found"},
+                     status_code=404,
+                     media_type="application/json")
